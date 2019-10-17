01b29caeb00675047cee1d52205ac787c987cd5b
github username <github email address>
2019-10-17 16:25:35 -0700

    updates

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import {taskMaster.jpg} from './pictures/taskMaster.jpg'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome to TaskMaster-5000</h1>
+
+    </div>
+  );
+}
+
+export default App;
